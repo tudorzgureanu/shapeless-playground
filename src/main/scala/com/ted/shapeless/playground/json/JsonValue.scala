a0bbf82d2cea80e686c7cee49f286b306a0d7ce8
Tudor Zgureanu <tzgureanu@gmail.com>
2016-12-31 18:30:20 +0200

    - Add Json AST model

---
+++ src/main/scala/com/ted/shapeless/playground/json/JsonValue.scala
@@ -0,0 +1,9 @@
+package com.ted.shapeless.playground.json
+
+sealed trait JsonValue
+case class JsonObject(fields: List[(String, JsonValue)]) extends JsonValue
+case class JsonArray(items: List[JsonValue]) extends JsonValue
+case class JsonString(value: String) extends JsonValue
+case class JsonNumber(value: Double) extends JsonValue
+case class JsonBoolean(value: Boolean) extends JsonValue
+case object JsonNull extends JsonValue
